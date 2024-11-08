4d1f11f92335fb6e6c9fe1370dea7ccd3286b537
Youmin Lee <80508023+JavaCPP0@users.noreply.github.com>
2024-11-08 11:18:29 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.58 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    
+    let result="";
+    
+    for(let i=1;i<=n;i++){
+        if(i%2===1){
+            result+="수";
+        } else if(i%2===0){
+            result+="박";
+        }
+    }
+    
+    return result;
+}
