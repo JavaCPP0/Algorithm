1fefaf07df8849de6d62ee327036b778a8d3709d
Youmin Lee <80508023+JavaCPP0@users.noreply.github.com>
2025-07-15 16:59:17 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
@@ -1,40 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <string>
 
 using namespace std;
 
-int main(void) {
+int main() {
+    int t;
+    cin >> t;
+    vector<int> units = { 25, 10, 5, 1 };
 
-	int t;
+    vector<int> arr(t);
+    for (int i = 0; i < t; i++) {
+        cin >> arr[i];
+    }
 
+    for (int j = 0; j < t; j++) {
+        int value = arr[j];
+        for (int unit : units) {
+            cout << value / unit << " ";
+            value %= unit;
+        }
+        cout << '\n';
+    }
 
-	cin >> t;
-
-	vector<int> arr(t);
-
-	for (int i = 0; i < t; i++) {
-		cin >> arr[i];
-	}
-
-	for (int j = 0; j < t; j++) {
-
-		string result = "";
-
-		result += to_string(arr[j] / 25) + " ";
-		arr[j] = arr[j] % 25;
-
-		result += to_string(arr[j] / 10) + " ";
-		arr[j] = arr[j] % 10;
-
-		result += to_string(arr[j] / 5) + " ";
-		arr[j] = arr[j] % 5;
-
-		result += to_string(arr[j] / 1) + " ";
-		arr[j] = arr[j] % 1;
-
-		cout << result << endl;
-	}
-
-	return 0;
+    return 0;
 }
