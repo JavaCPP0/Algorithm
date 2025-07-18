427681db86394b44beae8b8e6122493daa579dd1
Youmin Lee <80508023+JavaCPP0@users.noreply.github.com>
2025-07-18 21:32:41 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int x;
+
+	cin >> x;
+
+	int sum = 1;
+	int preSum = 0;
+	int line = 1;
+	int index = 0;
+	int numerator = 0;
+	int denominator = 0;
+
+	while (x > sum) { // 몇번째줄인가?
+		line++;
+		preSum = sum;
+		sum += line;
+	}
+
+	index = x - preSum; // i번째줄 몇번째인가?
+
+	if (line % 2 == 0) { //짝수라면
+		numerator = 1;
+		denominator = line; // ex)3번째줄 분모분자의 가장 큰수는 3이다.
+
+		for (int i = 1; i < index; i++) { //분자는 증가하고 분모는 감소한다.
+			numerator++;
+			denominator--;
+		}
+	}
+	else { //홀수라면
+		numerator = line;
+		denominator = 1;
+
+		for (int i = 1; i < index; i++) { //분모는 증가하고 분자는 감소한다.
+			numerator--;
+			denominator++;
+		}
+	}
+
+	cout << numerator << "/" << denominator;
+
+	return 0;
+}
