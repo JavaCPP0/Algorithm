34b7e02554dad0f4bc138631522b850df786d1f7
Youmin Lee <80508023+JavaCPP0@users.noreply.github.com>
2025-11-20 18:19:52 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(void) {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, k;
+	cin >> n >> k;
+
+	queue<int> q;
+
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+
+	cout << "<";
+
+	while (!q.empty()) {
+
+		for (int i = 0; i < k-1; i++) {
+			q.push(q.front());
+			q.pop();
+		}
+		
+		if (q.size() == 1) {
+			cout << q.front() << ">";
+			q.pop();
+		}
+		else {
+			cout << q.front() << ", ";
+			q.pop();
+		}
+
+	}
+
+
+	return 0;
+}
