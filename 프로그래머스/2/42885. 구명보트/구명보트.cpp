b0fbbe954ab57b28ad44beb0f01e49cb2eda5056
Youmin Lee <80508023+JavaCPP0@users.noreply.github.com>
2025-12-30 21:59:58 +0900

    [level 2] Title: 구명보트, Time: 1.80 ms, Memory: 4.77 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    
+    int answer = 0;
+    int left = 0, right = people.size()-1;
+    
+    sort(people.begin(),people.end());
+    
+    while (left <= right) {
+        if (people[left] + people[right] <= limit) {
+            left++;
+        }
+        right--;
+        answer++;
+    }
+    
+    return answer;
+}
